fef894a210217c3830ffb4479acdf7a8e8f58908
pkkim <paulkimpaul@gmail.com>
2015-04-08 18:17:39 -0500

    Forgot to commit addition of type annotations, also added a forgotten comma

--- SI.elm
+++ SI.elm
@@ -17,7 +17,7 @@ centi, milli, micro, nano, pico, femto, atto, zepto, yocto
 
 # SI named derived units
 @docs hertz, radian, steradian, newton, pascal, joule, watt, coulomb, volt,
-farad, ohm siemens, weber, tesla, henry, degree, lumen, lux, becquerel, gray,
+farad, ohm, siemens, weber, tesla, henry, degree, lumen, lux, becquerel, gray,
 sievert, katal
 -}
 
@@ -26,18 +26,25 @@ import Ratio (..)
 
 -- SI base units.
 {-| SI unit of length. -}
+meter : Unit
 meter = baseLength
 {-| SI unit of time. -}
+second : Unit
 second = baseTime
 {-| SI unit of mass. -}
+kilogram : Unit
 kilogram = baseMass
 {-| SI unit of temperature. -}
+kelvin : Unit
 kelvin = baseTemp
 {-| SI unit of amount. -}
+mole : Unit
 mole = baseAmount
 {-| SI unit of electric current. -}
+ampere : Unit
 ampere = baseCurrent
 {-| SI unit of luminous intensity. -}
+candela : Unit
 candela = baseLumInt
 
 -- Metric prefixes to scale units by some factor.
@@ -105,64 +112,90 @@ yocto = scale (10 ^ -24)
 -- Kilogram is the SI unit of mass, but we prefix "gram," not "kilogram."
 -- Defining "gram" allows us to say "milli gram," etc.
 {-| More commonly prefixed unit of mass. -}
+gram : Unit
 gram = milli kilogram
 
 -- Some units have more than one spelling.
 {-| Alternate spelling of meter. -}
+metre : Unit
 metre = meter
 {-| Alternate spelling of kilogram. -}
+kilogramme : Unit
 kilogramme = kilogram
 {-| Alternate spelling of gram. -}
+gramme : Unit
 gramme = gram
 
 -- SI named derived units.
 {-| Unit for frequency equal to one per second. -}
+hertz : Unit
 hertz = inv second
 {-| Unit for angle measure defined as the angle subtended on a circle of unit
 radius by an arc of unit length. Equal to 1/(2 pi) turns around a circle. -}
+radian : Unit
 radian = unity
 {-| Unit for solid angle measure defined as the solid angle subtended on a
 sphere of unit radius by a unit area on its surface. 4 pi steradians subtend a
 sphere. -}
+steradian : Unit
 steradian = unity
 {-| SI derived unit of force. -}
+newton : Unit
 newton = (kilogram `mul` meter) `per` (second `pow` (fromInt 2))
 {-| SI derived unit of pressure. -}
+pascal : Unit
 pascal = newton `per` (second `pow` (fromInt 2))
 {-| SI derived unit of energy. -}
+joule : Unit
 joule = newton `mul` meter
 {-| SI derived unit of power. -}
+watt : Unit
 watt = joule `per` second
 {-| SI derived unit of charge. -}
+coulomb : Unit
 coulomb = second `mul` ampere
 {-| SI derived unit of voltage. -}
+volt : Unit
 volt = watt `per` ampere
 {-| SI derived unit of capacitance. -}
+farad : Unit
 farad = coulomb `per` volt
 {-| SI derived unit of electric resistance. -}
+ohm : Unit
 ohm = volt `per` ampere
 {-| SI derived unit of electric conductance. -}
+siemens : Unit
 siemens = inv ohm
 {-| SI derived unit of magnetic flux. -}
+weber : Unit
 weber = joule `per` ampere
 {-| SI derived unit of magnetic flux density. -}
+tesla : Unit
 tesla = weber `per` (meter `pow` (fromInt 2))
 {-| SI derived unit of inductance. -}
+henry : Unit
 henry = volt `mul` second `per` ampere
 {-| SI derived unit of temperature, whose units are the same size as Kelvin and
 whose zero is 273.15 kelvin above absolute zero. -}
+degree : Unit
 degree = affineUnit 273.15 kelvin
 {-| SI derived unit of luminous intensity. -}
+lumen : Unit
 lumen = candela `mul` steradian
 {-| SI derived unit of luminous flux per area. -}
+lux : Unit
 lux = lumen `per` (meter `pow` fromInt 2)
 {-| SI derived unit of radioactivity. -}
+becquerel : Unit
 becquerel = inv second
 {-| SI derived unit of ionizing radiation dose. -}
+gray : Unit
 gray = joule `per` kilogram
 {-| SI derived unit of ionizing radiation dose, measuring health effects on the
 human body. -}
+sievert : Unit
 sievert = joule `per` kilogram
 {-| SI derived unit of catalytic activity. -}
+katal : Unit
 katal = mole `per` second
 
